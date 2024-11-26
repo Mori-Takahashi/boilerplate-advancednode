8d1fc2a635aa135ddf9510e0490f2e641adad860
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-11-26 13:07:45 +0000

    add mongodb & serializeUser, deserializeUser

--- server.js
+++ server.js
@@ -3,25 +3,38 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const session = require("express-session");
+const session = require('express-session');
 const passport = require('passport');
+const { ObjectID } = require('mongodb');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // Für FCC-Testzwecke
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   cookie: { secure: false }
 }));
+
 app.use(passport.initialize);
 app.use(passport.session);
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  });
+});
 
 app.route('/').get((req, res) => {
   res.render('index', { title: 'Hello', message: 'Please log in' });
